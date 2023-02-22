2afe8c7dee0952fd639f74ab11db34dfd4fd8995
Helge Stegemoen <helge@stegemoen.com>
2023-02-22 09:51:12 +0100

    Tests for all the lambda code

--- HamcrestsForCollections/src/test/kotlin/LambdaTests.kt
+++ HamcrestsForCollections/src/test/kotlin/LambdaTests.kt
@@ -1,46 +1,89 @@
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
 import org.hamcrest.MatcherAssert.*
-import org.hamcrest.CoreMatchers.*
-import org.junit.Before
+import org.hamcrest.Matchers.*
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
+// Source for hamcrest tests: https://www.baeldung.com/hamcrest-collections-arrays
 
 class ListHamcrestTestsKtTest {
-    var list: MutableList<String> = mutableListOf<String>("ab", "cd", "ef")
-
-    @Before
-    fun add_gh(){
-        list.add("gh")
-    }
-    @Test
-    fun testListHas_cd(): Unit { list.add("cd")
-        assertThat(list, hasItem("cd"))
-    }
-    @Test
-    fun testListHas_gh(): Unit { list.add("gh")
-        assertThat(list, hasItem("gh"))
-    }
-
+    private var list: MutableList<String> = mutableListOf("ab", "cd", "ef", "gh")
 
     @Test
-    fun testsListHasItems() {
+    fun testHasItem(){
+        assertThat(list, hasItem("ab"))
+        assertThat(list, not(hasItem("ij")))
+    }
+
+    @Test
+    fun testHasItems() {
         assertThat(list, hasItems("ab", "ef"))
+        assertThat(list, not(hasItems("ab", "ij")))
     }
 
     @Test
-    fun testListIsUpperCase(){
-        var result = list.map(){it.uppercase()}
+    fun testUsingMap(){
+        // using map:
+        val result = list.map { it.uppercase() }
         assertThat(result, hasItems("AB", "CD", "EF", "GH"))
     }
 
+
+    /* ------Check all elements in a collection ----------*/
+    @Test
+    fun testAllIElementsInList(){
+        val collection = listOf("ab", "cd", "ef")
+        assertThat(collection, contains("ab", "cd", "ef"))
+        // out of order
+        assertThat(collection, containsInAnyOrder("ab", "ef", "cd"))
+        // too many elements
+        assertThat(collection, not(contains("ab", "cd", "ef", "gh")))
+    }
+
+    @Test
+    fun testIfArrayAndListIsEmpty(){
+        val strings: MutableList<String> = mutableListOf()
+        assertThat(strings, empty())
+
+        val array: Array<String> = arrayOf()
+        assertThat(array, emptyArray())
+    }
+    /* ------ check sizes -----------------*/
+    @Test
+    fun checkListSize(){
+        val collection = listOf(1, 2, 3, 4, 5)
+        assertThat(collection, hasSize(5))
+        // ToDo: figure out how to use iterable in kotlin
+        //assertThat(collection, Matchers.<String> iterableWithSize(4))
+
+    }
+
+    /* ----------------- JoinToString ----------------*/
     @Test
     fun testJoinWithSeparator(){
-        var joinedList = list.joinToString(" ",
-            transform = { "$it" })
-        assertEquals("ab cd ef gh", joinedList)
+        val collection = listOf("a", "b", "c", "d")
+        val list1: String = collection.joinToString(" ") { "[$it]" }
+        assertEquals(list1,"[a] [b] [c] [d]")
+
+        val joinedList = list.joinToString(" ",
+            transform = { "[$it]" })
+        assertEquals("[ab] [cd] [ef] [gh]", joinedList)
+    }
+    /* ----------------- mapIndexed ---------------*/
+    @Test
+    fun testMapIndexed(){
+        val chars = listOf('a','b','c','d')
+        assertThat(
+            chars.mapIndexed{index, element -> "${index}: $element"},
+            contains("0: a", "1: b", "2: c", "3: d"))
+    }
+    /* ---------------- ignore argument ---------- */
+    @Test
+    fun useOnlyIndex_IgnoreValueArgument(){
+        assertThat(list.mapIndexed { index, _ -> "$index"  } // _ => ignore compiler warning about unused identifiers
+       , contains("0","1","2","3"))
+
+        // or use list.indices instead
+        assertThat(list.indices.map { "$it" }
+            ,contains("0","1","2","3"))
     }
 
     @Test
